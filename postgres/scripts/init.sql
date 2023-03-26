7d078232fcfaaa8a02e79636ee242fe951d79879
Alex Alekseev <alexrace55@ya.ru>
2023-03-26 18:52:31 +0300

    Create postgres

---
+++ postgres/scripts/init.sql
@@ -0,0 +1,17 @@
+--CREATE DATABASE ocr_db;
+
+CREATE TABLE pictures
+(
+    id                  BIGSERIAL       PRIMARY KEY,
+    nickname            VARCHAR(255),
+    time_application    TIMESTAMP,
+    file_name           VARCHAR(255),
+    file_path           VARCHAR(255)
+);
+
+CREATE TABLE texts_from_pictures
+(
+    id                  BIGSERIAL       PRIMARY KEY,
+    id_picture          BIGSERIAL       REFERENCES pictures ON DELETE CASCADE,
+    text_from_picture   VARCHAR
+);
